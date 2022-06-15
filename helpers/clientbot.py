355308b2a82a482e91dfaf12fd35f8dabcada0f3
Sanssss <77996464+Gaclexxx@users.noreply.github.com>
2022-06-15 14:45:13 +0700

    Update and rename clientsbot.py to clientbot.py

--- helpers/clientbot.py
+++ helpers/clientbot.py
@@ -7,8 +7,8 @@ from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import InputStream
 from pytgcalls.types.input_stream import InputAudioStream
-from modules.config import API_ID, API_HASH, STRING_SESSION, STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9
-from . import queues
+from config import *
+from helpers import queues
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
