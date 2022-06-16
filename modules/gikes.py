c04c2675a5696023f13f236ea051a780fd9b4387
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-16 17:33:01 +0700

    Update gikes.py

--- modules/gikes.py
+++ modules/gikes.py
@@ -18,8 +18,8 @@ async def gikes(c: Client, m: Message):
     err_str, done_broadcast = "", 0
 
     async for dialog in c.iter_dialogs():
-    if dialog.chat.type in ["supergroup", "channel"]:
-        chats.append(dialog.chat.id)
+          if dialog.chat.type in ["supergroup", "channel"]:
+                chats.append(dialog.chat.id)
           try:
                 await c.send_message(dialog.chat.id, msg, disable_web_page_preview=True)
                 done_broadcast += 1
