3e9dc3fe8b7bf0613dd6418a7d7cc963ea34c624
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-17 02:05:43 +0700

    Update tools.py

--- modules/tools.py
+++ modules/tools.py
@@ -186,48 +186,6 @@ def choose_random_font():
     return wget.download(random_s)
 
 
-@Client.on_message(filters.command("logo", ["~", "!", "°"]) & filters.me)
-async def logo(client: Client, message: Message):
-    event = await edit_or_reply(message, "`Processing.....`")
-    text = get_text(message)
-    if not text:
-        await event.edit(
-            "`Please Give Me A Valid Input. You Can Check Help Menu To Know More!`"
-        )
-        return
-    font_ = choose_random_font()
-    img = Image.open("./bot_utils_files/image_templates/black_blank_image.jpg")
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(font_, 220)
-    image_widthz, image_heightz = img.size
-    w, h = draw.textsize(text, font=font)
-    h += int(h * 0.21)
-    draw.text(
-        ((image_widthz - w) / 2, (image_heightz - h) / 2),
-        text,
-        font=font,
-        fill=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    )
-    file_name = "LogoBy@Peler.png"
-    await client.send_chat_action(message.chat.id, "upload_photo")
-    img.save(file_name, "png")
-    if message.reply_to_message:
-        await client.send_photo(
-            message.chat.id,
-            photo=file_name,
-            caption="By Peler Userbot",
-            reply_to_message_id=message.reply_to_message.message_id,
-        )
-    else:
-        await client.send_photo(
-            message.chat.id, photo=file_name, caption="By Peler Userbot"
-        )
-    await client.send_chat_action(message.chat.id, "cancel")
-    await event.delete()
-    if os.path.exists(file_name):
-        os.remove(file_name)
-
-
 add_command_help(
     "tools",
     [
@@ -235,6 +193,5 @@ add_command_help(
         ["tgm", "Reply to Media as args to upload it to telegraph."],
         
         ["clone|unclone", "To Clone someone Profile."],
-        ["logo", "To make a random logo."],
     ],
 )
