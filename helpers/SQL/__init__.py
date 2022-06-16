590adea447e876b6a46ff618408e82e3cb93690e
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-17 06:22:50 +0700

    Update __init__.py

--- helpers/SQL/__init__.py
+++ helpers/SQL/__init__.py
@@ -1,3 +1,54 @@
+import os
+import motor.motor_asyncio
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from main import DB_URL, MONGO_DB
+
+
+mongo_dbb = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB)
+
+dbb = mongo_dbb["SPAMBOT"]
+
+SPAMBOT = 'SPAMBOT'
+def start() -> scoped_session:
+    engine = create_engine(DB_URL)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+def start() -> scoped_session:
+    dbi_url=DB_URL
+    engine = create_engine(dbi_url)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    print(
+        "DB_URI is not configured. Features depending on the database might have issues."
+    )
+    print(str(e))
+
+
+DB_AVAILABLE = False
+BOTINLINE_AVAIABLE = False
+
+def mulaisql() -> scoped_session:
+    global DB_AVAILABLE
+    engine = create_engine(DB_URL, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    try:
+        BASE.metadata.create_all(engine)
+    except exc.OperationalError:
+        DB_AVAILABLE = False
+        return False
+    DB_AVAILABLE = True
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 
