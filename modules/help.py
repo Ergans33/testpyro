a153c0743ff3c14203c7aa9bfd5e4fc2995a67ca
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-16 15:49:49 +0700

    Update help.py

--- modules/help.py
+++ modules/help.py
@@ -23,9 +23,9 @@ async def module_help(client: Client, message: Message):
         all_commands = ""
         all_commands += "Please specify which module you want help for!! \nUsage: `.help [module_name]`\n\n"
 
-        ac = PrettyTable()
+        ac = 💩()
         ac.header = False
-        ac.title = "Peler Modules"
+        ac.title = "👉🏽Peler Modules👈🏽"
         ac.align = "l"
 
         for x in split_list(sorted(CMD_HELP.keys()), 2):
