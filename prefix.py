7318b8352aa7e7ab424d9eddf7c364700db3242a
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-16 20:11:58 +0700

    Update prefix.py

--- prefix.py
+++ prefix.py
@@ -17,7 +17,7 @@ def my_prefix():
         prefix = get_prefix()
     except configparser.NoSectionError:
         config.add_section("prefix")
-        config.set("prefix", "prefix", "!")
+        config.set("~", "!", "°")
         with open(config_path, "w") as config_file:
             config.write(config_file)
         prefix = "~", "!", "°"
