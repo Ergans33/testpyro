48f0988d17ef3b748c29b32a4ac18cba9eebe3da
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-16 20:17:34 +0700

    Update gikes.py

--- modules/gikes.py
+++ modules/gikes.py
@@ -1,12 +1,13 @@
-from pyrogram import Client , filters
 import asyncio
+from pyrogram import Client , filters
 from pyrogram.types import Message
 from io import BytesIO, StringIO
 from modules.help import add_command_help
+from prefix import my_prefix
+prefix = my_prefix()
 
 
-
-@Client.on_message(filters.command(["gikes"], "!") & filters.me)
+@Client.on_message(filters.command(["gikes"], prefixes=prefix) & filters.me)
 async def gikes(c: Client, m: Message):
     if m.reply_to_message:
         msg = m.reply_to_message.text.markdown
