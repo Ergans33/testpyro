d4b9b5cfe96e3717532c8a76cfbec64af6fed4ea
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-18 04:52:27 +0700

    Update gikes.py

--- modules/gikes.py
+++ modules/gikes.py
@@ -17,8 +17,8 @@ async def iter_chats(client: Client):
 async def gbroadcast(client: Client, message: Message):
     msg_ = await message.edit_text("`Processing..`")
     failed = 0
-    await msg_.edit_text("{message.text}")
     if not message.reply_to_message:
+        await msg_.edit_text(message)
         await msg_.edit("`Reply To Message Boss!`")
         return
     chat_dict = await iter_chats(client)
