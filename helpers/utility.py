c7c5fbfb2e8ae30b32ccc89a0c039e624f6ca88b
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-17 21:27:11 +0700

    Update utility.py

--- helpers/utility.py
+++ helpers/utility.py
@@ -23,7 +23,7 @@ async def make_carbon(code):
     url = "https://carbonara.vercel.app/api/cook"
     async with aiosession.post(url, json={"code": code}) as resp:
         image = BytesIO(await resp.read())
-    image.name = "carbon.png"
+    image.name = "carbon.jpg"
     return image
     
 
