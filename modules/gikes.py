33fed8aec01d531827837282ad1aee27ec3176e3
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-18 05:39:12 +0700

    Update gikes.py

--- modules/gikes.py
+++ modules/gikes.py
@@ -13,7 +13,7 @@ async def gbroadcast(client: Client, message: Message):
         msg_ = await message.edit_text("`Processing..`")
         failed = 0
     else:
-        msg_edit_text = ""
+        msg_.edit_text = ""
     if not message.reply_to_message:
         await msg_.edit("`Reply To Message Boss!`")
         return
