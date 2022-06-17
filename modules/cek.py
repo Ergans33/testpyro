92a2471dc119a1432403dc3f70ff7455eb96c153
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-17 19:20:34 +0700

    Update cek.py

--- modules/cek.py
+++ modules/cek.py
@@ -1,10 +1,10 @@
 import asyncio
 from pyrogram import Client
 from pyrogram.types import Message
-from config import 
+from config import SUDO_USERS as SUDO_USER
 
 
 
-@Client.on_message(filters.user(SUDO_USER) & filters.command(["gangsta", "gang", "gangstar"], ["."]) & filters.me)
+@Client.on_message(filters.user(SUDO_USER) & filters.command("aktif", ["?"]) & filters.me)
 async def gajjajay(client: Client, message: Message):
-        await message.edit("")		
+        await message.edit("Peler Ubot Nyala Gess🎃")		
