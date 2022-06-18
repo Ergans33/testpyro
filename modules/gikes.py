a4dd553c3e54e20f250757b9d61213fbab926469
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-18 23:32:30 +0700

    Update gikes.py

--- modules/gikes.py
+++ modules/gikes.py
@@ -22,7 +22,7 @@ async def gbroadcast(client: Client, message: Message):
     
     for c in chat_dict:
         try:
-            await message.send_message(c)
+            await await client.send_message(msg)
         except:
             failed += 1
     await msg_.edit(
