aa366bfdb2a811b1f1648e329516c437cc12fee7
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-18 20:42:03 +0700

    Update gikes.py

--- modules/gikes.py
+++ modules/gikes.py
@@ -1,35 +1,38 @@
 import asyncio
+from pyrogran import FloodWait
 from pyrogram import Client , filters
 from pyrogram.types import Message
 from helpers.gban_errors import iter_chats
 from helpers.basic import get_text
 from modules.help import add_command_help
 
-    
-    
+ 
 @Client.on_message(filters.me & filters.command("gikes", ["~", "!", "°"]))
 async def gbroadcast(client: Client, message: Message):
-    if xx := get_text(message)
+    if xx == get_text(message)
         msg = xx
     elif message.reply_to_message:
         msg = message.reply_to_message
     else:
         return await message.reply_text("Input text or Reply to a message")
     msg_ = await message.reply_text("`Processing..`")
-    failed =0
-    chat_dict = await iter_chats(client)
-    chat_len = len(chat_dict)
-    await msg_.edit("`Now Sending To All Chats Possible!`")
-    if not chat_dict:
-        msg_.edit("`You Have No Chats! So Sad`")
-        return
-    for c in chat_dict:
-        try:
-            msg = await message.reply_to_message.copy(c)
-        except:
+    failed = 0
+    done = 0
+    async for x in client.inter.dialogs()
+        if chat = x.id
+            if chat not in GCAST_BLACKLIST
+                try:
+                    await client.send_message(chat, msg)
+                    await asyncio.sleep(0.1)
+                    done += 1
+                except FloodWait as kntl
+                    await asyncio.sleep(int(kntl.seconds))
+                    await client.send_message(chat, msg)
+                    done += 1
+                except:
             failed += 1
     await msg_.edit(
-        f"`Message Sucessfully Send To {chat_len-failed} Chats! Failed In {failed} Chats.`"
+        f"`Message Sucessfully Send To {done} Chats! Failed In {failed} Chats.`"
     )
 
 
