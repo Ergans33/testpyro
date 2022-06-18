6d65753e82a3812f93db2ae4b32c11adc213a73c
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-18 22:29:02 +0700

    Update gikes.py

--- modules/gikes.py
+++ modules/gikes.py
@@ -19,10 +19,6 @@ async def gbroadcast(client: Client, message: Message):
     failed =0
     done =0
     async for x in client.iter_dialogs():
-        if x.is_group:
-            chat = x.id
-            if chat not in GCAST_BLACKLIST:
-                try:
                     await client.send_message(chat, msg)
                     await asyncio.sleep(0.1)
                     done += 1
