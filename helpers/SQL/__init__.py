8fdc330b4809353d90f6b7d9e6a7343c8b78241d
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-20 03:14:16 +0700

    Update __init__.py

--- helpers/SQL/__init__.py
+++ helpers/SQL/__init__.py
@@ -1,7 +1,21 @@
-
-
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_DB
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from userbot import DB_URI
+
+BASE = declarative_base()
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+SESSION = start()
 
 mongodb = AsyncIOMotorClient(MONGO_DB)
 dbb = mongodb["SPAMBOT"]
