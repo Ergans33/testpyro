3cc5ab7bdec4790bd1763ce66b81d93eb856e608
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-20 05:06:27 +0700

    Update gmute.py

--- modules/gmute.py
+++ modules/gmute.py
@@ -16,14 +16,10 @@ async def gmute_him(client: Client, message: Message):
         try:
             if message.reply_to_message:
                 me_ = message.reply_to_message.from_user.id
-            return await message.edit("`You Don't Have Permission! To mute`")
+            return await message.edit("`You Don't Have Permission! To mute`"
+    )
 
     
-
-
-
-
-
 add_command_help(
     "gmute",
     [
