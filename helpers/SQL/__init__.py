13a4b572ff2f57ef42073a6ca971c581f693e340
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-20 04:48:14 +0700

    Update __init__.py

--- helpers/SQL/__init__.py
+++ helpers/SQL/__init__.py
@@ -1,15 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_DB
+from config import *
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from userbot import DB_URI
 
 BASE = declarative_base()
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI)
+    engine = create_engine(DB_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
