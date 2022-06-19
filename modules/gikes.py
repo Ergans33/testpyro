1c3ea45765525816694b7d894b85d3dc9ca7b675
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-06-19 15:29:10 +0700

    Update gikes.py

--- modules/gikes.py
+++ modules/gikes.py
@@ -9,7 +9,7 @@ from modules.help import add_command_help
  
 @Client.on_message(filters.me & filters.command("gikes", ["~", "!", "°"]))
 async def gbroadcast(client: Client, message: Message):
-    if text_ := get_text(message):
+    if text_ = get_text(message):
         msg = text_
     elif message.reply_to_message:
         msg = message.reply_to_message.text
